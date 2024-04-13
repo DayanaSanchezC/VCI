057675ea1c102a8de3048f2a7648ac9801c73291
Dayana <sanchezcabello@outlook.es>
2024-04-12 21:49:12 -0600

    v1

---
+++ Token.java
@@ -0,0 +1,18 @@
+public class Token {
+    String lexema;
+    String token;
+    String pts;
+    String linea;
+
+    public Token(String lexema, String token, String pts, String linea) {
+        this.lexema = lexema;
+        this.token = token;
+        this.pts = pts;
+        this.linea = linea;
+    }
+
+    @Override
+    public String toString() {
+        return lexema + "," + token + "," + pts + "," + linea;
+    }
+}
